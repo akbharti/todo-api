ceca44c4a5027deebd7847222063cc3eef0f39f0
Abhishek bharti <dashing.abhi517@gmail.com>
2019-07-22 14:50:52 +0530

    add id field

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 let http = require("http");
 let bodyParser = require('body-parser');
 let express = require("express");
+let _ = require("underscore");
+
 let app = express();let PORT = 3000;
 let todos = [];
 let todoNextId = 1;
@@ -22,29 +24,39 @@ app.get('/todos/:id',(req,res) => {
 // res.send(`Asking for todo with id of ${req.params.id}`);
    let todoid = parseInt(req.params.id,10);
 
-   //iterate of todos array. find the match
-    todos.forEach((item,index) =>{
-       if(item.id == todoid)
-         res.json(todos[index]);
-    })
+ var matchedTodo = _.findWhere(todos,{id:todoid});
 
-   res.status(404).send("Page not found");
+
+ matchedTodo ?  res.json(todos[todoid]) : res.status(404).send("Page not found");
+
+//    //iterate of todos array. find the match
+//     todos.forEach((item,index) =>{
+//        if(item.id == todoid)
+//          res.json(todos[index]);
+//     })
+
+//    res.status(404).send("Page not found");
 })
 
-//POST
+//POST todo
 
 app.post('/todos',(req,res)=>{
      let body = req.body;
 
-//add id field
-body.id = todoNextId++;
-//push body into array
-todos.push(body);
+if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length == 0)
+{
+    return res.status(400).send("Bad request");
+}
+
+    //add id field
+    body.id = todoNextId++;
+
+    //push body into array
+    todos.push(body);
      
      res.json(body);  
-
 });
-
+ 
 
 app.listen(PORT,()=>{
     console.log(`Express server is on port no. ${PORT}`);
