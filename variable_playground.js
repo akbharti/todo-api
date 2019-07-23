86b4805c925915695be422b93bf099eeb6cd8863
Abhishek bharti <dashing.abhi517@gmail.com>
2019-07-23 18:20:46 +0530

    Add variable playground

---
+++ variable_playground.js
@@ -0,0 +1,28 @@
+// let person = {
+//     name : "Abhishek",
+//     age : 21
+// };
+
+// function update(obj){
+//     // obj = {
+//     //     name : "Abhishek",
+//     //     age : 24
+//     // };
+//  obj.age  =24; 
+// }
+
+// update(person);
+// console.log(person);
+
+//Array example
+let grade = [15,88];
+
+function addgrade(gradeArr){
+    gradeArr = [1,2,3];
+   debugger;
+
+   // gradeArr = [12,33,99]
+} 
+
+addgrade(grade)
+console.log(grade);
